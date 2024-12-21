5cd9a7aa54b1adfb8a4a72ddd6fb4b7a1c182780
Kromvin <131769841+Kromvin@users.noreply.github.com>
2024-12-21 17:09:58 +0500

    3

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -2,48 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use App\DTOs\ServerInfoDTO;
-use App\DTOs\ClientInfoDTO;
-use App\DTOs\DatabaseInfoDTO;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class InfoController extends Controller
 {
-
-    public function getServerInfo()
+    public function serverInfo()
     {
-        // Сохраняем результат phpinfo() в строку
-        ob_start();
-        phpinfo(INFO_GENERAL); // Вывод только общей информации
-        $phpInfo = ob_get_clean();
-    
-        // Удаляем HTML-теги и очищаем специальные символы
-        $cleanedPhpInfo = strip_tags($phpInfo);
-    
-        // Создаем DTO
-        $dto = new ServerInfoDTO(
-            phpVersion: phpversion(),
-        );
-    
-        return response()->json($dto->toArray());
-    }
-    
-    
-
-    public function getClientInfo(Request $request)
-    {
-        $dto = new ClientInfoDTO(
-            ip: $request->ip(),
-            userAgent: $request->header('User-Agent', 'unknown')
-        );
-
-        return response()->json($dto->toArray());
+        return response()->json([
+            'phpinfo' => phpversion()
+        ]);
     }
 
-    public function getDatabaseInfo()
+    public function clientInfo(Request $request)
     {
-        $dto = DatabaseInfoDTO::fromConfig();
+        return response()->json([
+            'ip' => $request->ip(),
+            'userAgent' => $request->userAgent()
+        ]);
+    }
 
-        return response()->json($dto->toArray());
+    public function databaseInfo(){
+        return response()->json([
+            'database' => DB::connection()->getDatabaseName()
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InfoController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/info/server', [InfoController::class, 'getServerInfo']);
-Route::get('/info/client', [InfoController::class, 'getClientInfo']);
-Route::get('/info/database', [InfoController::class, 'getDatabaseInfo']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/info/server', [InfoController::class, 'serverInfo']);
+
+Route::get('/info/client', [InfoController::class, 'clientInfo']);
+
+Route::get('/info/database', [InfoController::class, 'databaseInfo']);
